6b392da9f602a99407c8dbb582c7a12a873c6e7d
Capn-Cast0rBean <87210224+Capn-Cast0rBean@users.noreply.github.com>
2021-07-11 18:46:21 -0400

    Added Files
    Added the main Python file (PythonTwitterBotTemplate.py).

---
+++ PythonTwitterBotTemplate.py
@@ -0,0 +1,42 @@
+""" IMPORTS """
+
+
+
+import time
+import tweepy
+
+
+
+
+
+""" OUTPUT FUNCTION """
+
+
+
+def twitterPost():
+
+
+
+
+
+""" TWITTER OUTPUT """
+
+
+
+CONSUMER_KEY = 'Enter your Consumer Key here.'
+CONSUMER_SECRET = 'Enter your Consumer Secret Key here.'
+ACCESS_KEY = 'Enter your Access Key here.'
+ACCESS_SECRET = 'Enter your Access Secret Key here.'
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+
+'''
+while True:
+    postthis = twitterPost()
+    if len(postthis) <= 140:
+        api.update_status(status=postthis)
+        print(postthis)
+        time.sleep(3600)
+'''
