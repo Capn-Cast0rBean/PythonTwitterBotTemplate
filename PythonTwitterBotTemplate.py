3f9135390cdabe1ee5a0b6c5f11c69dc56f6de88
Capn-Cast0rBean <87210224+Capn-Cast0rBean@users.noreply.github.com>
2021-07-11 23:36:01 -0400

    Update PythonTwitterBotTemplate.py
    Reformatted the comments and added a variable that determined the amount of hours between each tweet.

--- PythonTwitterBotTemplate.py
+++ PythonTwitterBotTemplate.py
@@ -1,6 +1,4 @@
-""" IMPORTS """
-
-
+### IMPORTS ###
 
 import time
 import tweepy
@@ -9,34 +7,31 @@ import tweepy
 
 
 
-""" OUTPUT FUNCTION """
-
-
+### OUTPUT FUNCTION ###
 
 def twitterPost():
+    return ("This bot will tweet the return value of the function twitterPost().")
 
 
 
 
 
-""" TWITTER OUTPUT """
+### TWITTER OUTPUT ###
 
-
+hoursBetweenTweet = 1
 
 CONSUMER_KEY = 'Enter your Consumer Key here.'
 CONSUMER_SECRET = 'Enter your Consumer Secret Key here.'
 ACCESS_KEY = 'Enter your Access Key here.'
 ACCESS_SECRET = 'Enter your Access Secret Key here.'
+
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
-
-'''
 while True:
     postthis = twitterPost()
     if len(postthis) <= 140:
         api.update_status(status=postthis)
         print(postthis)
-        time.sleep(3600)
-'''
+        time.sleep(hoursBetweenTweet * 3600)
